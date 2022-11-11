f5be3dc43896023043de869c2a2b03e10fbf5757
abukati <artiomb7@gmail.com>
2022-11-11 12:49:58 +0200

    updated manifest

--- Package.swift
+++ Package.swift
@@ -1,1 +1,33 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
+import Foundation
+
+let package = Package(
+  name: "RustShopping",
+  platforms: [
+    .iOS(*)
+  ],
+  products: [
+    .library(
+      name: "RustShopping",
+      targets: ["RustShopping"]
+    )
+  ],
+  targets: [
+    .target(
+      name: "RustShopping",
+      dependencies: ["ShoppingBackend"]
+    )
+    .binaryTarget(
+      name: "RustShopping",
+      url: "https://github.com/abukati/rust-swift/bundle.zip",
+      checksum: "97d6eb3425c3bcab090900cce385d00113206187b98b190ba74f1b27ed4f7150"
+    )
+    .testTarget(
+      name: "RustShopping",
+      dependencies: ["RustShopping"]
+    )
+  ]
+)
