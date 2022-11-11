80b70d835fe03bbcc87bf3d155ff5cc667bbbf5d
abukati <artiomb7@gmail.com>
2022-11-11 12:53:11 +0200

    update

--- Package.swift
+++ Package.swift
@@ -18,16 +18,16 @@ let package = Package(
   targets: [
     .target(
       name: "RustShopping",
-      dependencies: ["ShoppingBackend"]
+      dependencies: ["RustShopping"]
     )
     .binaryTarget(
       name: "RustShopping",
       url: "https://github.com/abukati/rust-swift/bundle.zip",
       checksum: "97d6eb3425c3bcab090900cce385d00113206187b98b190ba74f1b27ed4f7150"
     )
-    .testTarget(
-      name: "RustShopping",
-      dependencies: ["RustShopping"]
-    )
+    // .testTarget(
+    //   name: "RustShopping",
+    //   dependencies: ["RustShopping"]
+    // )
   ]
 )
