e2760e4d9638ee307225c1f417f5cd040d97780c
abukati <artiomb7@gmail.com>
2022-11-13 09:27:33 +0200

    d

--- Package.swift
+++ Package.swift
@@ -11,17 +11,17 @@ let package = Package(
         ],
         products: [
             .library(
-                name: "ShopScraper",
-                targets: ["ShopScraper"]),
+                name: "ShoppingBackend",
+                targets: ["ShoppingBackend"]),
         ],
         targets: [
 //            .target(
 //                name: "ShopScraper",
 //                dependencies: ["ShoppingBackend"]),
             .binaryTarget(
-                name: "ShopScraper",
-                url: "https://github.com/abukati/rust-swift/bundle.zip",
-                checksum: "22913f75ea6b98ab74b4757d9c0692331716050272da7731e537218d8382cc82"),
+                name: "ShoppingBackend",
+                url: "https://github.com/abukati/rust-swift/archive/refs/tags/pub.zip",
+                checksum: "41a314d6d8549c511764acdf439d5bce58dbac8b28031dae31afafb21587f831"),
             .testTarget(
                 name: "ShoppingBackendTests",
                 dependencies: ["ShoppingBackend"]),
